9ff47da4da84cb84e9cdf78ba1b7d9625a59f5d7
WinnterWolf <patrick.winter@outlook.com.br>
2020-10-13 14:30:02 -0400

    testes para o heroku

--- src/main/java/com/acme/app/main/SimpleServlet.java
+++ src/main/java/com/acme/app/main/SimpleServlet.java
@@ -3,6 +3,7 @@ package com.acme.app.main;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.*;
 import javax.servlet.ServletException;
+import javax.ws.rs.GET;
 import java.io.*;
 
 
@@ -17,5 +18,9 @@ public class SimpleServlet extends HttpServlet {
         response.setContentType("text/plain");
         response.setStatus(HttpServletResponse.SC_OK);
         response.getWriter().println("Simple servlet");
+    }
+    @GET
+    public String getTest(){
+        return "API test!";
     }
 }
